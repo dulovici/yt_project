64c468422834086e25a42c7336275b3a90527308
BIT Student <igorx24@gmail.com>
2021-06-04 11:01:17 +0200

    iife

--- script.js
+++ script.js
@@ -1,82 +1,88 @@
 
-'use strict'
+
 
 // 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=BMW&key=[YOUR_API_KEY]'
 // const key = 'AIzaSyDSXjGS5-H-o6Dn7e1DWrh3mfho7HAZ1n4';
 
+(function () {
+    'use strict'
 
-const main = document.querySelector('main');
-const input = document.querySelector('input');
-const nextBtn = document.querySelector('.next');
-let inputValue;
+    const main = document.querySelector('main');
+    const input = document.querySelector('input');
+    const nextBtn = document.querySelector('.next');
+    let inputValue;
 
-//PARAMETARS FOR NUMBER & CHOISE OF CARDS
-const numOfCards = 12;
-let currentNumOfCards = 12;
+    //PARAMETARS FOR NUMBER & CHOISE OF CARDS
+    const numOfCards = 12;
+    let currentNumOfCards = 12;
 
 
 
-//GETTING DATA FROM API
-function getData() {
-    const req = new XMLHttpRequest();
-    req.open('GET', `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${currentNumOfCards}&q=${inputValue}&key=AIzaSyDSXjGS5-H-o6Dn7e1DWrh3mfho7HAZ1n4`);
-    req.send();
-    req.onload = function () {
-        const data = JSON.parse(req.responseText).items;
-        console.log(data);
-        if (data.length === numOfCards) generatePage(data);
-        else generatePage(data.splice(data.length - numOfCards, data.length));
+    //GETTING DATA FROM API
+    function getData() {
+        const req = new XMLHttpRequest();
+        req.open('GET', `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${currentNumOfCards}&q=${inputValue}&key=AIzaSyDSXjGS5-H-o6Dn7e1DWrh3mfho7HAZ1n4`);
+        req.send();
+        req.onload = function () {
+            const data = JSON.parse(req.responseText).items;
+            console.log(data);
+            if (data.length === numOfCards) generatePage(data);
+            else generatePage(data.splice(data.length - numOfCards, data.length));
+        }
     }
-}
 
-//CREATING HTML FROM RECIVED DATA
-function generatePage(apiData) {
-    apiData.forEach(e => {
-        const card = document.createElement('div');
-        card.setAttribute('class', 'card');
-        const img = document.createElement('img');
-        img.src = `${e.snippet.thumbnails.high.url}`;
-        const h2 = document.createElement('h2');
-        h2.textContent = `${e.snippet.title}`;
-        const par = document.createElement('p');
-        par.textContent = `${e.snippet.description}`;
-        const video = document.createElement('iframe');
-        video.src = `https://www.youtube.com/embed/${e.id.videoId}`;
-        video.setAttribute('class', 'player')
+    //CREATING HTML FROM RECIVED DATA
+    function generatePage(apiData) {
+        apiData.forEach(e => {
+            const card = document.createElement('div');
+            card.setAttribute('class', 'card');
+            const img = document.createElement('img');
+            img.src = `${e.snippet.thumbnails.high.url}`;
+            const h2 = document.createElement('h2');
+            h2.textContent = `${e.snippet.title}`;
+            const par = document.createElement('p');
+            par.textContent = `${e.snippet.description}`;
+            const video = document.createElement('iframe');
+            video.src = `https://www.youtube.com/embed/${e.id.videoId}`;
+            video.setAttribute('class', 'player')
 
-        main.append(card);
-        card.append(img);
-        card.append(h2);
-        card.append(par)
+            main.append(card);
+            card.append(img);
+            card.append(h2);
+            card.append(par)
 
-        card.addEventListener('click', function (e) {
-            main.append(video)
+            card.addEventListener('click', function (e) {
+                main.append(video)
+            })
         })
+    }
+
+    //=======EVENTS========//
+    input.addEventListener("keydown", function (e) {
+        if (e.keyCode === 13) {
+            main.innerHTML = '';
+            inputValue = input.value;
+            getData();
+            input.value = '';
+        }
     })
-}
 
-//=======EVENTS========//
-input.addEventListener("keydown", function (e) {
-    if (e.keyCode === 13) {
+    nextBtn.addEventListener('click', function (e) {
         main.innerHTML = '';
-        inputValue = input.value;
+        currentNumOfCards += numOfCards;
         getData();
-        input.value = '';
-    }
-})
 
-nextBtn.addEventListener('click', function (e) {
-    main.innerHTML = '';
-    currentNumOfCards += numOfCards;
-    getData();
+    })
 
-})
+    document.addEventListener('keydown', function (e) {
+        if (e.keyCode === 27) {
+            const player = document.querySelector('iframe');
+            player.classList.add('hide')
+        }
+    })
 
-document.addEventListener('keydown', function (e) {
-    if (e.keyCode === 27) {
-        const player = document.querySelector('iframe');
-        player.classList.add('hide')
-    }
-})
 
-//test
+
+})()
+
+
